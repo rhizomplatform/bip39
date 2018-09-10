cc5d3d7a31cfee574a402f5d64a3ba6eef90fcd9
Rafael Escrich <rafaelescrich@gmail.com>
2018-09-10 10:44:13 -0300

    Add typings

---
+++ index.ts
@@ -0,0 +1,24 @@
+export const wordlists: {
+    EN: string[];
+    JA: string[];
+    chinese_simplified: string[];
+    chinese_traditional: string[];
+    brazilian_portuguese: string[];
+    english: string[];
+    french: string[];
+    italian: string[];
+    japanese: string[];
+    spanish: string[];
+};
+
+export function entropyToMnemonic(entropyHex: string, wordlist?: string[]): string;
+
+export function generateMnemonic(strength?: number, rng?: (size: number) => Buffer, wordlist?: string[]): string;
+
+export function mnemonicToEntropy(mnemonic: string, wordlist?: string[]): string;
+
+export function mnemonicToSeed(mnemonic: string, password?: string): Buffer;
+
+export function mnemonicToSeedHex(mnemonic: string, password?: string): string;
+
+export function validateMnemonic(mnemonic: string, wordlist?: string[]): boolean;
